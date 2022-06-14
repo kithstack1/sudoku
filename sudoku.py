e88de4c172e2a1e733fa7c840f4b0e7669dcbff8
kithstack1 <21903316@emu.edu.tr>
2022-06-14 22:14:51 +0300

    edited sudoku file

--- sudoku.py
+++ sudoku.py
@@ -14,39 +14,39 @@ empty_cells = {}
 FILLED = '*'
 # VALUES = list(range(1,10))
 VALUES = list(string.ascii_uppercase[0:25])
-BOARD = [[EMPTY for _ in range(len(VALUES))] for _ in range(len(VALUES))]
-grid_len = int((len(BOARD)**0.5))
+board = [[EMPTY for _ in range(len(VALUES))] for _ in range(len(VALUES))]
+grid_len = int((len(board)**0.5))
 pset = {}
-PFILLED = int((len(BOARD)**2)*(PFILLED/100))
+PFILLED = int((len(board)**2)*(PFILLED/100))
 
 # draw board to screen
 def draw_board():
-    global grid_len, BOARD
+    global grid_len, board
     
-    for i in range(len(BOARD)):
+    for i in range(len(board)):
         row_data = []
-        for j in range(len(BOARD)):
-            if (not j%grid_len) and (j != len(BOARD)-1) and j != 0:
-                cell_data = f'| {BOARD[i][j]}'
+        for j in range(len(board)):
+            if (not j%grid_len) and (j != len(board)-1) and j != 0:
+                cell_data = f'| {board[i][j]}'
             else:
-                cell_data = f'  {BOARD[i][j]}'
+                cell_data = f'  {board[i][j]}'
             row_data.append(cell_data)    
             print(cell_data,end='') 
         print('',end='\n')
-        if (not (i+1)%grid_len) and i!= 0 and i != len(BOARD)-1:
+        if (not (i+1)%grid_len) and i!= 0 and i != len(board)-1:
             print('-'*len(''.join(row_data)))
     print()
 
 
 def check_criteria(value,row,col):
-    global BOARD, grid_len
+    global board, grid_len
     # vertical check
-    for i in range(len(BOARD)):
-        if BOARD[i][col] == value:
+    for i in range(len(board)):
+        if board[i][col] == value:
             return False
     # horizontal check
-    for j in range(len(BOARD)):
-        if BOARD[row][j] == value:
+    for j in range(len(board)):
+        if board[row][j] == value:
             return False
     # box check
     x0 = col//grid_len * grid_len
@@ -54,16 +54,16 @@ def check_criteria(value,row,col):
 
     for row in range(y0,y0+grid_len):
         for col in range(x0,x0+grid_len):
-            if BOARD[row][col] == value:
+            if board[row][col] == value:
                 return False
     return True
 
 def make_empty():
-    global BOARD, PFILLED
+    global board, PFILLED
     i = 0
     p_filled_set = []
     while i < PFILLED:
-        row, col = randint(0,len(BOARD)-1), randint(0,len(BOARD)-1)
+        row, col = randint(0,len(board)-1), randint(0,len(board)-1)
         cell = (row,col)
         if cell in p_filled_set:
             continue
@@ -74,15 +74,15 @@ num = 0
 
 
 def prefill():
-    global num, VALUES, BOARD
-    for row in range(len(BOARD)):
-        for col in range(len(BOARD)):
-            if BOARD[row][col] == '*':
+    global num, VALUES, board
+    for row in range(len(board)):
+        for col in range(len(board)):
+            if board[row][col] == '*':
                 for value in VALUES:
                     if check_criteria(value, row, col):
-                        BOARD[row][col] = value
+                        board[row][col] = value
                         prefill()
-                        BOARD[row][col] = '*'
+                        board[row][col] = '*'
                 return
     if num == 0:
         pset_init()
@@ -91,21 +91,21 @@ def prefill():
     solve_board()
 
 def init_board():
-    global empty_cells, BOARD, EMPTY, VALUES
+    global empty_cells, board, EMPTY, VALUES
     to_be_filled = make_empty()
 
 
     for cell in to_be_filled:
         row, col = cell[0], cell[1]
-        BOARD[row][col] = '*'
+        board[row][col] = '*'
     
 
 def pset_init():
-    global BOARD
-    for row in range(len(BOARD)):
+    global board
+    for row in range(len(board)):
         empty_cells[row] = {}
-        for col in range(len(BOARD)):
-            if BOARD[row][col] == EMPTY:
+        for col in range(len(board)):
+            if board[row][col] == EMPTY:
                 empty_cells[row][col] = []
 
 
@@ -116,10 +116,10 @@ def pset_init():
 # and the entire row and also find restricted cells
 def gen_pset():
     # generate all possible values for each cell ahead of time
-    global EMPTY, BOARD, VALUES, empty_cells
-    for row in range(len(BOARD)):
-        for col in range(len(BOARD)):
-            if BOARD[row][col] == EMPTY:
+    global EMPTY, board, VALUES, empty_cells
+    for row in range(len(board)):
+        for col in range(len(board)):
+            if board[row][col] == EMPTY:
                 for value in VALUES:
                     if check_criteria(value, row, col):
                         draw_board()
@@ -129,7 +129,7 @@ def gen_pset():
                     # got a singleton
                     print('GOT A SINGLETON')
                     # sleep(4)
-                    BOARD[row][col] = empty_cells[row][col][0]
+                    board[row][col] = empty_cells[row][col][0]
                     del empty_cells[row][col]
                 elif len(empty_cells[row][col]) == 0:
                     # board is unsolvable
@@ -146,15 +146,15 @@ def gen_pset():
     print(empty_cells)
 
 def solve_board():
-    global BOARD, empty_cells, EMPTY, tFILLED
-    for row in range(len(BOARD)):
-        for col in range(len(BOARD)):
-            if BOARD[row][col] == EMPTY:
+    global board, empty_cells, EMPTY, tFILLED
+    for row in range(len(board)):
+        for col in range(len(board)):
+            if board[row][col] == EMPTY:
                 for value in empty_cells[row][col]: 
                     if check_criteria(value,row,col):
-                        BOARD[row][col] = value
+                        board[row][col] = value
                         solve_board()
-                        BOARD[row][col] = EMPTY
+                        board[row][col] = EMPTY
                 return # all values incompatible , return to parent caller
     print('board is solved , no more empty slots')
     draw_board()
