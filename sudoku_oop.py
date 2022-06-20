2eaef2b272ea16e6e6d122003de2d82add7d55d8
kithstack1 <21903316@emu.edu.tr>
2022-06-21 02:39:10 +0300

    corrected single position logic error for block check

--- sudoku_oop.py
+++ sudoku_oop.py
@@ -1,14 +1,15 @@
-from random import randint, choice
-import os
+from random import randint
 from time import sleep
 import string
+from colorama import Fore 
 
+GREEN, WHITE = Fore.GREEN, Fore.WHITE
 
 
 
 # the set of values used for the sudoku , could be letters or numbers , or anything else
 # none of these should be repeated , like i said , it is a set .. allowed set lengths are 9,16,12,25,100[the mega sudoku]
-VALUES = list(string.ascii_uppercase[0:9])
+VALUES = list(string.ascii_uppercase[0:16])
 # a var to mark the cells for solving
 EMPTY = '#'
 # the sudoku board , it will be a list of lists{length of each will be the length of the values set}
@@ -16,7 +17,7 @@ EMPTY = '#'
 # each list representing a row , all lists of equal length # and each index in list representing a cell ie intersection of a row and a column 
 board = [[EMPTY for _ in range(len(VALUES))] for _ in range(len(VALUES))]
 # what percentage of the board is has givens 
-PFILLED = 5
+PFILLED = 20
 PFILLED = int((len(board)**2)*(PFILLED/100))
 empty_cells = {}
 # the FILLED var will be used by the givens generator code to know which
@@ -31,6 +32,8 @@ grid_len = int((len(board)**0.5))
 # possible occupants... 
 pset = {}
 
+# trying to splve number of spaces hit by logic and not bruteforce
+logic = 0
 # draw board to screen
 def draw_board():
     global grid_len, board
@@ -88,6 +91,11 @@ def make_empty():
 # this var is to ensure generation of a pset only once
 num = 0
 
+# add some green formatting to the board solution inputs
+def pprint(value):
+    return f'{GREEN}{value}{WHITE}'
+
+
 # the givens generator code.. since this program will be for now
 # solving sudokus it generates on its own.. the code will as well
 # call the solver function immediately after making a board..
@@ -121,7 +129,7 @@ def init_board():
         row, col = cell[0], cell[1]
         board[row][col] = FILLED
     
-
+# prepare candidate set hashmap for each empty cell
 def pset_init():
     global board, EMPTY
     for row in range(len(board)):
@@ -138,7 +146,7 @@ def pset_init():
 # same applies to the entire row and also find restricted cells
 def gen_pset():
     # generate all possible values for each cell ahead of time
-    global EMPTY, grid_len,  board, VALUES, empty_cells
+    global EMPTY, logic, grid_len,  board, VALUES, empty_cells
     for row in range(len(board)):
         for col in range(len(board)):
             if board[row][col] == EMPTY:
@@ -151,6 +159,7 @@ def gen_pset():
                     print('GOT A SINGLETON')
                     # force the value into the board
                     board[row][col] = empty_cells[row][col][0]
+                    logic += 1
                     # delete the cell from empty_cells
                     del empty_cells[row][col]
                     # clear any occerence of the value in p_candidates in the cell's block , row and column
@@ -196,7 +205,7 @@ def x_wing():
 
 # the single position optimisation
 def single_position():
-    global empty_cells, VALUES, board
+    global empty_cells, logic,  VALUES, board
     # the single position
     # row check
     for row in empty_cells:
@@ -208,6 +217,7 @@ def single_position():
         for value in value_map:
             if value_map[value][0] == 1: 
                 board[row][value_map[value][1][0]] = value
+                logic += 1
                 # delete the cell from empty_cells hash map
                 cell = value_map[value][1][0]
                 del empty_cells[row][cell]
@@ -226,6 +236,7 @@ def single_position():
             if value_map[value][0] == 1:
                row = value_map[value][1][0]
                board[row][col] = value
+               logic += 1
                del empty_cells[row][col]
                update_pset(row,col,value)
     # block check
@@ -242,16 +253,15 @@ def single_position():
                             value_map[value][1].append((r,c))
         for value in value_map:
             if value_map[value][0] == 1: 
-                cell = value_map[value][1][0][1]
-                board[row][cell] = value
+                cell_cord = value_map[value][1][0]
+                r,c = cell_cord[0], cell_cord[1]
+                board[r][c] = value
+                logic += 1
                 # delete the cell from empty_cells hash map
                 try:
-                   print('works like a charm')
-                   sleep(34)
                    del empty_cells[row][cell]
                 except Exception as e:
                     print('error',e,empty_cells[row],cell)
-                    sleep(23)
                 # delete any occurence of the value in the p_candidates in the block its in
                 # the row its in and the column          
                 update_pset(row,cell,value)
@@ -304,7 +314,7 @@ def update_pset(row, col, value):
 # after all solving techniques have been tried ... and 
 # p_candidates for each cell reduced to min size
 def solve_board():
-    global board, empty_cells, EMPTY 
+    global board, empty_cells, EMPTY , logic
     for row in range(len(board)):
         for col in range(len(board)):
             if board[row][col] == EMPTY:
@@ -315,6 +325,7 @@ def solve_board():
                         board[row][col] = EMPTY
                 return # all values incompatible , return to parent caller
     print('board is solved , no more empty slots')
+    print(f'{logic} number of cells was filled using logic')
     yield(draw_board())
 
 def main():
